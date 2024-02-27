0aa5f02a1838a9ef0a0b80d30377ad837adbdf0a
Enrique De Ridder <riamap23@gmail.com>
2024-02-27 17:37:29 +0100

    feat: vue setup

---
+++ frontend/src/components/Header/HeaderMenu.vue
@@ -0,0 +1,20 @@
+<template>
+	<header>
+		<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+		<div class="wrapper">
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/about">About</RouterLink>
+			</nav>
+		</div>
+	</header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped lang="scss">
+
+</style>
